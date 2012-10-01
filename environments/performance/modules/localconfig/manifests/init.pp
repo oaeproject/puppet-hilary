6fb7b3b5fe8c7d1048027dfc011e99b30a930bc7
Branden Visser <mrvisser@gmail.com>
2012-10-01 13:16:16 -0400

    Node download

--- environments/performance/modules/localconfig/manifests/init.pp
+++ environments/performance/modules/localconfig/manifests/init.pp
@@ -7,4 +7,8 @@ class localconfig {
   # App servers
   $app0_ip = '165.225.130.148'
 
+
+  # Dependency versions
+  $node_version = '0.8.11'
+
 }
--- environments/performance/modules/localconfig/manifests/nodes.pp
+++ environments/performance/modules/localconfig/manifests/nodes.pp
@@ -4,5 +4,13 @@ node 'app0' inherits appnode {
     ensure   => present,
     provider => pkgin,
   }
-
+  
+  $node_version = $localconfig::node_version
+  $node_dl = "/tmp/node-v$node_version.tar.gz"
+  $node_dir = "/opt/node/${localconfig::node_version}"
+  
+  exec { 'node_wget':
+    command => "/usr/bin/wget http://nodejs.org/dist/v${node_version}/node-v${node_version}.tar.gz -O $node_dl",
+    unless  => "test -f $node_dir"
+  }
 }
