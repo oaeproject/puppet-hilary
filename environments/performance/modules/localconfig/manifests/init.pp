f7a667fc641c9314099d5fdb7268299dc5245649
Branden Visser <mrvisser@gmail.com>
2012-10-01 16:13:57 -0400

    Adding app config template

--- environments/performance/modules/localconfig/manifests/init.pp
+++ environments/performance/modules/localconfig/manifests/init.pp
@@ -5,8 +5,21 @@ class localconfig {
   $group   = 'staff'
   
   # App servers
-  $app0_ip = '165.225.130.148'
+  $app_hosts = ['165.225.130.148']
 
+  # Redis
+  $redis_hosts = ['127.0.0.1']
+  $redis_port = 6379
+  $redis_dbIndex = 0
+  
+  # Cassandra
+  $db_keyspace = 'oae'
+  $db_hosts = [ '127.0.0.1' ]
+  $db_timeout = 5000
+  $db_replication = 1
+  $db_strategyClass = 'SimpleStrategy'
+
+  # Install details
   $app_root = '/opt/oae'
   $app_git_user = 'sakaiproject'
   $app_git_branch = 'master'
--- environments/performance/modules/localconfig/manifests/nodes.pp
+++ environments/performance/modules/localconfig/manifests/nodes.pp
@@ -2,12 +2,12 @@ node 'app0' inherits appnode {
 
   package { 'nodejs':
     ensure    => present,
-    provider  => pkgin
+    provider  => pkgin,
   }
   
   package { 'scmgit':
     ensure    => present,
-    provider  => pkgin
+    provider  => pkgin,
   }
   
   vcsrepo { "${localconfig::app_root}":
@@ -17,4 +17,15 @@ node 'app0' inherits appnode {
     revision  => "${localconfig::app_git_branch}",
     require   => Package['scmgit'],
   }
+  
+  exec { "chown_${localconfig::app_root}":
+    command => "/opt/local/bin/chown -R ${localconfig::user}:${localconfig::group} ${localconfig::app_root}",
+    require => Vcsrepo["${localconfig::app_root}"],
+  }
+  
+  file { "${localconfig::app_root}/config.js":
+    ensure  => present,
+    content => template('localconfig/config.js.erb'),
+  }
+
 }
