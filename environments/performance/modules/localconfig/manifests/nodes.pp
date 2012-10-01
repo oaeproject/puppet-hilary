6eb6bdcf13390fc1ddda09be36e3c50a2917999c
Branden Visser <mrvisser@gmail.com>
2012-10-01 13:42:31 -0400

    Adding make && make install

--- environments/performance/modules/localconfig/manifests/nodes.pp
+++ environments/performance/modules/localconfig/manifests/nodes.pp
@@ -6,7 +6,8 @@ node 'app0' inherits appnode {
   }
   
   $node_version = $localconfig::node_version
-  $node_dl = "/tmp/node-v$node_version.tar.gz"
+  $node_src = "/tmp/node-v${node_version}"
+  $node_dl = "${node_src}.tar.gz"
   $node_root = '/opt/node'
   $node_dir = "$node_root/${localconfig::node_version}"
   
@@ -19,13 +20,24 @@ node 'app0' inherits appnode {
   
   exec { "node_${node_version}_wget":
     command => "/usr/bin/wget http://nodejs.org/dist/v${node_version}/node-v${node_version}.tar.gz -O ${node_dl}",
-    unless  => "/opt/local/bin/test -f $node_dir"
+    unless  => "/opt/local/bin/test -f ${node_dir}/bin/node",
   }
   
   exec { "node_${node_version}_extract":
     cwd     => "/tmp",
     command => "/opt/local/bin/tar -zxvf ${node_dl}",
+    unless  => "/opt/local/bin/test -f ${node_dir}/bin/node",
   }
     
-    
+  exec { "node_${node_version}_configure":
+    cwd     => "${node_src}"
+    command => "./configure --prefix=${node_dir}"
+    unless  => "/opt/local/bin/test -f ${node_dir}/bin/node",
+  }
+  
+  exec { "node_${node_version}_make":
+    cwd     => "${node_src}"
+    command => "/opt/local/bin/make && /opt/local/bin/make install"
+    unless  => "/opt/local/bin/test -f ${node_dir}/bin/node",
+  }
 }
