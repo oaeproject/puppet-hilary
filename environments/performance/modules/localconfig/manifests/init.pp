cd1e35497eb510151f887ee2ad0031d5010be11d
Branden Visser <mrvisser@gmail.com>
2012-10-04 07:29:51 -0400

    Set 3-node cassandra hosts

--- environments/performance/modules/localconfig/manifests/init.pp
+++ environments/performance/modules/localconfig/manifests/init.pp
@@ -17,7 +17,7 @@ class localconfig {
   # Cassandra
   $db_cluster_name = 'Sakai OAE Performance Testing Cluster'
   $db_keyspace = 'oae'
-  $db_hosts = [ '127.0.0.1' ]
+  $db_hosts = [ '10.112.2.10', '10.112.4.67', '10.112.4.83' ]
   $db_timeout = 5000
   $db_replication = 1
   $db_strategyClass = 'SimpleStrategy'
