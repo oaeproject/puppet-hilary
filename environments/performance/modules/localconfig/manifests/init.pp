b5711c2006daac070c66a1f3c0789ea804f467ce
Branden Visser <mrvisser@gmail.com>
2012-10-05 06:59:09 -0400

    Updating host config for actual cluster.

--- environments/performance/modules/localconfig/manifests/init.pp
+++ environments/performance/modules/localconfig/manifests/init.pp
@@ -7,11 +7,11 @@ class localconfig {
   $db_group   = 'root' 
   
   # Web servers
-  $web_hosts = ['165.225.133.100']
+  $web_hosts = ['165.225.133.115']
   
   # App servers
-  $app_hosts_internal = ['']
-  $app_hosts_external = ['165.225.130.148']
+  $app_hosts_internal = ['10.112.4.121', '10.112.4.122']
+  $app_hosts_external = ['165.225.133.113', '165.225.133.114']
 
   # Redis
   $redis_hosts = [$app_hosts[0]]
@@ -19,7 +19,7 @@ class localconfig {
   # Cassandra
   $db_cluster_name = 'Sakai OAE Performance Testing Cluster'
   $db_keyspace = 'oae'
-  $db_hosts = [ '10.112.2.10', '10.112.4.67', '10.112.4.83' ]
+  $db_hosts = [ '10.112.4.124', '10.112.4.125', '10.112.4.126' ]
   $db_timeout = 5000
   $db_replication = 1
   $db_strategyClass = 'SimpleStrategy'
