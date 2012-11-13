ec337e99896616c87cbdce3195501ad1e8d5f3bc
Simon Gaeremynck <gaeremyncks@gmail.com>
2012-11-13 10:04:09 +0000

    Added config for redis

--- environments/performance/modules/redis/manifests/init.pp
+++ environments/performance/modules/redis/manifests/init.pp
@@ -5,11 +5,22 @@ class redis {
     provider  => pkgin,
   }
 
+  # Set the configuration file.
+  file { 'redis.conf':
+    path => '/opt/local/etc/redis.conf',
+    ensure => present,
+    mode => 0640,
+    owner => $owner,
+    group => $group,
+    content => template('redis/redis.conf.erb'),
+    require => Package['redis'],
+  }
+
   # define the service to restart
   service { 'redis':
     ensure  => 'running',
     enable  => 'true',
-    require => Package['redis'],
+    require => File['redis.conf'], Package['redis'],
   }
   
 }
