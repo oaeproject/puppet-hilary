f84cb15084fd08f044ec51181dd41445e9701923
Branden Visser <mrvisser@gmail.com>
2013-02-11 06:58:07 -0500

    Setting eviction back on to redis for further investigation. Also better puppeting redis startup.

--- environments/performance/modules/redis/manifests/init.pp
+++ environments/performance/modules/redis/manifests/init.pp
@@ -5,22 +5,28 @@ class redis {
     provider  => pkgin,
   }
 
+  exec { 'svccfg import redis.xml':
+    command => 'svccfg import /opt/local/share/smf/manifest/redis.xml',
+    require => Package['redis'],
+  }
+
   # Set the configuration file.
   file { 'redis.conf':
-    path => '/opt/local/etc/redis.conf',
-    ensure => present,
-    mode => 0640,
-    owner => $owner,
-    group => $group,
+    path    => '/opt/local/etc/redis.conf',
+    ensure  => present,
+    mode    => 0640,
+    owner   => $owner,
+    group   => $group,
     content => template('redis/redis.conf.erb'),
-    require => Package['redis'],
+    require => Package['redis']
   }
 
   # define the service to restart
   service { 'redis':
-    ensure  => 'running',
-    enable  => 'true',
-    require => File['redis.conf']
+    ensure    => 'running',
+    enable    => 'true',
+    require   => File['redis.conf'],
+    subscribe => File['redis.conf']  
   }
   
 }
