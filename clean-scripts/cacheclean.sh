7aa9479d06a3b1e9b7f51a6d22fc018a2b041f6a
Branden Visser <mrvisser@gmail.com>
2013-02-19 13:13:44 -0500

    Flush after puppet, so redis is installed.

--- clean-scripts/cacheclean.sh
+++ clean-scripts/cacheclean.sh
@@ -2,10 +2,10 @@
 # This bash script holds the commands to scrub
 # a redis instance and start fresh
 
-# Flush the redis data
-redis-cli flushall
-
 # Pull latest puppet config and apply it.
 cd /home/admin/puppet-hilary
 bin/pull.sh
 sudo bin/apply.sh
+
+# Flush the redis data
+redis-cli flushall
