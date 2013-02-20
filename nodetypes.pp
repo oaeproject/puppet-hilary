91fc405e0bc0d5c0419a980b294738a4b91d8ccd
Simon Gaeremynck <gaeremyncks@gmail.com>
2013-02-20 17:08:36 +0000

    Support for shibboleth authentication.

--- nodetypes.pp
+++ nodetypes.pp
@@ -24,6 +24,16 @@ node appnode inherits basenode {
   ## INSTALL HILARY AND 3AKAI-UX CONTAINER ##
   ###########################################
 
+  package { "sun-jre6-6.0.26":
+    ensure    => present,
+    provider  => 'pkgin',
+  }
+
+  exec { "pull_in_saml_jar":
+    command => "/usr/bin/wget --directory-prefix=/opt http://stuff.gaeremynck.com/oae/org.sakaiproject.Hilary.SAMLParser-1.0-SNAPSHOT-jar-with-dependencies.jar",
+    creates => "/opt/org.sakaiproject.Hilary.SAMLParser-1.0-SNAPSHOT-jar-with-dependencies.jar",
+  }
+
   class { 'hilary':
     app_root_dir        => $localconfig::app_root,
     app_git_user        => $localconfig::app_git_user,
