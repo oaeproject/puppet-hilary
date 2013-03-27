95d956509e3d51db67d0c51a6a4f4c9d21084662
Branden Visser <mrvisser@gmail.com>
2013-03-27 16:33:20 -0400

    Fix centos provisionin issue

--- provisioning/before-reboot/centos.sh
+++ provisioning/before-reboot/centos.sh
@@ -5,13 +5,13 @@ then
 fi
 
 SCRIPT_ENVIRONMENT=$1
-SCRIPT_HOST=$2
+SCRIPT_HOSTNAME=$2
 SCRIPT_PUPPET_INTERNAL_IP=$3
 
 # set HOSTNAME in /etc/sysconfig/network and /etc/hosts
 sed -i -e "s/localhost.localdomain/$SCRIPT_HOSTNAME/g" /etc/sysconfig/network
 sed -i "s/localhost /$SCRIPT_HOSTNAME localhost /g" /etc/hosts
 echo "$SCRIPT_HOSTNAME" > /etc/hostname
-echo "$SCRIPT_PUPPET_INTERNAL_IP puppet" > /etc/hosts
+echo "$SCRIPT_PUPPET_INTERNAL_IP puppet" >> /etc/hosts
 
 echo "Pre-reboot setup complete. Please reboot the machine VIA the Joyent admin UI, then run the after-reboot script."
