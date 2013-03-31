c42bd91aca26d1aa0f86dcaef01fbc3c35f6f910
Branden Visser <mrvisser@gmail.com>
2013-03-31 11:29:18 -0400

    Fix bad comma

--- modules/oaeservice/manifests/firewall/open.pp
+++ modules/oaeservice/manifests/firewall/open.pp
@@ -3,7 +3,7 @@ class oaeservice::firewall::open {
   case $operatingsystem {
     solaris, Solaris: {
       class { 'ipfilter': rules => [ 'pass in quick', 'pass out quick' ] }
-    },
+    }
     default: {
       include oaeservice::firewall
       iptables { '001 allow all input':   chain => 'INPUT',   iniface => 'eth0', jump => 'ACCEPT' }
