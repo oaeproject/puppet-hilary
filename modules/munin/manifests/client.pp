22b00b10faf288159afccfb177499393e1d85e94
Branden Visser <mrvisser@gmail.com>
2013-03-31 12:36:01 -0400

    Fix munin

--- modules/munin/manifests/client.pp
+++ modules/munin/manifests/client.pp
@@ -3,15 +3,17 @@ class munin::client ($hostname) {
   case $operatingsystem {
     solaris, Solaris: {
       $provider = 'pkgin'
+      $etc_dir = '/opt/local/etc'
     }
     default: {
       $provider = undef
+      $etc_dir = '/etc'
     }
   }
 
   package { 'munin-node': ensure => installed, provider => $provider }
 
-  file { '/etc/munin/munin-node.conf':
+  file { "$etc_dir/munin/munin-node.conf":
     ensure  => present,
     mode    => '0644',
     content => template('munin/munin-node.conf.erb'),
@@ -20,7 +22,7 @@ class munin::client ($hostname) {
 
   service { 'munin-node':
     ensure  => running,
-    require => [ File['/etc/munin/munin-node.conf'] ]
+    require => [ File["$etc_dir/munin/munin-node.conf"] ]
   }
 
 }
