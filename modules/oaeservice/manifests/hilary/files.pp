a35e8f78c9c6e4a46fe07b5efd47396d59375122
Branden Visser <mrvisser@gmail.com>
2013-04-01 09:51:32 -0400

    Fix app

--- modules/oaeservice/manifests/hilary/files.pp
+++ modules/oaeservice/manifests/hilary/files.pp
@@ -1,7 +1,8 @@
 class oaeservice::hilary::files {
   $hilary_files = hiera('app_files_nfs')
+  $app_files_parent = hiera('app_files_parent')
 
-  smartosnfs { hiera('app_files_parent'):
+  smartosnfs { $app_files_parent:
     server     => $hilary_files['server'],
     sourcedir  => $hilary_files['sourcedir']
   }
