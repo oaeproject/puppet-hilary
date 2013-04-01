0b8c9efd939ced045b2016d7b5a5fe57f79f9df3
Branden Visser <mrvisser@gmail.com>
2013-04-01 17:04:32 -0400

    Fix phantomjs install

--- modules/oaeservice/manifests/hilary/pp.pp
+++ modules/oaeservice/manifests/hilary/pp.pp
@@ -5,20 +5,26 @@ class oaeservice::hilary::pp {
 
   package { $pp_packages: ensure => installed }
 
-  # Checkout the tip of the 1.9 phantomjs branch
-  vcsrepo { '/tmp/phantomjs':
-    provider    => 'git',
-    ensure      => 'present',
-    source      => 'https://github.com/ariya/phantomjs.git',
-    revision    => 'da71c5fbddafbef5c033fd6cd4a916ab3c9fd548',
-    unless      => 'test -x /usr/local/bin/phantomjs'
+  # Checkout a deterministic tip of the 1.9 phantomjs branch. Cannot use vcsrepo as we need 'unless' functionality
+  exec { 'clone_phantomjs':
+    cwd     => '/tmp',
+    command => 'git clone https://github.com/ariya/phantomjs.git',
+    unless  => 'test -x /usr/local/bin/phantomjs',
+    require => Package['git'],
+  }
+
+  exec { 'checkout_phantomjs':
+    cwd     => '/tmp/phantomjs',
+    command => 'git reset --hard da71c5fbddafbef5c033fd6cd4a916ab3c9fd548',
+    unless  => 'test -x /usr/local/bin/phantomjs',
+    require => Exec['clone_phantomjs'],
   }
 
   exec { '/tmp/phantomjs/build.sh':
     cwd         => '/tmp/phantomjs',
     command     => './build.sh',
     unless      => 'test -x /usr/local/bin/phantomjs',
-    require     => [ Package[$pp_packages], Package['build-essential'], Package['git'], Vcsrepo['/tmp/phantomjs'] ],
+    require     => [ Package[$pp_packages], Package['build-essential'], Package['git'], Exec['checkout_phantomjs'] ],
     before      => Class['::hilary'],
   }
 
