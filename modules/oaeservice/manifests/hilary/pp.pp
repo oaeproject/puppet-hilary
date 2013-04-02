f853b8f85005dc36ee3d1e652e905a25eb946784
Branden Visser <mrvisser@gmail.com>
2013-04-02 07:49:10 -0400

    Fix phantomjs

--- modules/oaeservice/manifests/hilary/pp.pp
+++ modules/oaeservice/manifests/hilary/pp.pp
@@ -4,9 +4,9 @@ class oaeservice::hilary::pp {
   $pp_packages = [ 'libreoffice', 'pdftk', 'chrpath', 'libssl-dev', 'libfontconfig1-dev' ]
   package { $pp_packages: ensure => installed }
 
-  archive { 'phantomjs.tar.bz2':
+  archive { 'phantomjs':
     ensure      => present,
-    url         => 'https://phantomjs.googlecode.com/files/phantomjs-1.9.0-linux-x86_64',
+    url         => 'https://phantomjs.googlecode.com/files/phantomjs-1.9.0-linux-x86_64.tar.bz2',
     target      => '/opt',
     extension   => 'tar.bz2',
     src_target  => '/opt',
