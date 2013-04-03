67c9a6448a1578ac7b9c2ff0f0d14324d831921b
Branden Visser <mrvisser@gmail.com>
2013-04-03 11:22:17 -0400

    Fix colon

--- modules/oaeservice/manifests/deps/package/java6.pp
+++ modules/oaeservice/manifests/deps/package/java6.pp
@@ -3,8 +3,8 @@ class oaeservice::deps::package::java6 {
         ubuntu, debian: {
             package { 'openjdk-6-jdk': ensure => installed }
         }
-        solaris, Solaris {
-            exec { "java_notsupported": command   => fail("No Java support yet for ${::operatingsystem}") }
+        solaris, Solaris: {
+            exec { "java_notsupported": command => fail("No Java support yet for ${::operatingsystem}") }
         }
         default: {
             package { 'java-1.6.0-openjdk-devel': ensure => installed }
