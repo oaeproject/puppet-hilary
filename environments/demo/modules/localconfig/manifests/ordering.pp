29d0f928c9652454d6d0f00b86b955cb2a251a36
Branden Visser <mrvisser@gmail.com>
2013-04-04 06:53:27 -0400

    Fix component ordering

---
+++ environments/demo/modules/localconfig/manifests/ordering.pp
@@ -0,0 +1,12 @@
+class localconfig::ordering {
+
+    ## All these components should be installed before hilary
+    Class['::oaeservice::ui']               -> Class['::oaeservice::hilary']
+    Class['::redis']                        -> Class['::oaeservice::hilary']
+    Class['::oaeservice::elasticsearch']    -> Class['::oaeservice::hilary']
+    Class['::oaeservice::cassandra']        -> Class['::oaeservice::hilary']
+    Class['::oaeservice::mq']               -> Class['::oaeservice::hilary']
+
+    ## After the app server is installed and ready, then setup nginx
+    Class['::oaeservice::hilary']           -> Class['::oaeservice::nginx']
+}
