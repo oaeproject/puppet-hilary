7b1a1fa450adc13d51327d96753f2437740fec0d
Branden Visser <mrvisser@gmail.com>
2013-04-04 09:44:48 -0400

    Fix elasticsearch

--- modules/oaeservice/manifests/elasticsearch.pp
+++ modules/oaeservice/manifests/elasticsearch.pp
@@ -22,6 +22,7 @@ class oaeservice::elasticsearch {
     min_memory_mb     => hiera('search_memory_mb'),
     path_data         => hiera('search_data_dir'),
     version           => hiera('search_version'),
+    checksum          => hiera('search_checksum'),
     rsyslog_enabled   => $rsyslog_enabled,
     rsyslog_host      => $rsyslog_host,
   }
