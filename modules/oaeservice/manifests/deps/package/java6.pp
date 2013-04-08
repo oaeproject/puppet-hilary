0807dbab79727392ac78d6433d677c466d3e72ed
Branden Visser <mrvisser@gmail.com>
2013-04-08 14:29:25 -0400

    Accept java license

--- modules/oaeservice/manifests/deps/package/java6.pp
+++ modules/oaeservice/manifests/deps/package/java6.pp
@@ -4,7 +4,8 @@ class oaeservice::deps::package::java6 {
             package { 'openjdk-6-jdk': ensure => installed }
         }
         solaris, Solaris: {
-            package { 'sun-jre6-6.0.26': ensure => installed, provider => 'pkgin' }
+            exec { 'accept_java_license': command => 'touch /opt/local/.dlj_license_accepted' }
+            package { 'sun-jre6-6.0.26': ensure => installed, provider => 'pkgin', require => Exec['accept_java_license'] }
         }
         default: {
             package { 'java-1.6.0-openjdk-devel': ensure => installed }
