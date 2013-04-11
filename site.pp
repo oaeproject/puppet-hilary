65ebfbd877e6382235dfc3f0de5742bc1b21aeac
Branden Visser <mrvisser@gmail.com>
2013-04-11 17:19:38 -0400

    Removing traces of epel and cross-platform paths)

--- site.pp
+++ site.pp
@@ -1,27 +1,11 @@
 
-##########
-## PATH ##
-##########
+## Set the path globally
 
-case $operatingsystem {
-  debian, ubuntu: {
-    $defaultPath = ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin']
-  }
-  solaris, Solaris: {
-    $defaultPath = ['/opt/local/gnu/bin', '/opt/local/bin', '/opt/local/sbin', '/usr/bin', '/usr/sbin']
-  }
-  default: {
-    $defaultPath = ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin']
-  }
-}
-Exec { path => $defaultPath }
-
+Exec { path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'] }
 
 ###############
 ## EXECUTION ##
 ###############
-
-include epel
 
 # Instantiate the nodetypes and the environment-specific node config
 import 'nodetypes.pp'
