d09c0f582ab99c4d7a2c212bf2aa36ac4dfe4362
Branden Visser <mrvisser@gmail.com>
2013-04-11 16:30:12 -0400

    Fix variable substitution

--- modules/oaeservice/manifests/deps/package/nodejs.pp
+++ modules/oaeservice/manifests/deps/package/nodejs.pp
@@ -6,14 +6,14 @@ class oaeservice::deps::package::nodejs ($nodejs_version, $npm_version, $nodegyp
   apt::ppa { 'ppa:chris-lea/node.js': }
   apt::ppa { 'ppa:chris-lea/node.js-legacy': }
 
-  package { "nodejs=$nodejs_version": ensure => installed, require => Class['apt'] }
-  package { "npm=$npm_version": ensure => installed, require => Class['apt'] }
+  package { "nodejs=${nodejs_version}": ensure => installed, require => Class['apt'] }
+  package { "npm=${npm_version}": ensure => installed, require => Class['apt'] }
 
   # Force the npm bundled version of node-gyp to upgrade node-gyp. Needed to build node-expat and hiredis
   exec { 'npm_reinstall_nodegyp':
-    command   => "npm install node-gyp@$nodegyp_version",
+    command   => "npm install node-gyp@${nodegyp_version}",
     cwd       => '/usr/lib/nodejs/npm',
     logoutput => 'on_failure',
-    require   => Package["npm=$npm_ubuntu_version"],
+    require   => Package["npm=${npm_version}"],
   }
 }
