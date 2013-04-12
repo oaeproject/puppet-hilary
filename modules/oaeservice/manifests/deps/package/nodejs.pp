30a2ce557212a90a2284dfe7d7f89ea935f3570a
Branden Visser <mrvisser@gmail.com>
2013-04-12 10:31:01 -0400

    Controlling package order

--- modules/oaeservice/manifests/deps/package/nodejs.pp
+++ modules/oaeservice/manifests/deps/package/nodejs.pp
@@ -6,15 +6,29 @@ class oaeservice::deps::package::nodejs ($nodejs_version, $npm_version, $nodegyp
   apt::ppa { 'ppa:chris-lea/node.js': }
   apt::ppa { 'ppa:chris-lea/node.js-legacy': }
 
-  package { "nodejs=${nodejs_version}": ensure => installed, require => Class['apt'] }
-  package { "nodejs-dev=${nodejs_version}": ensure => installed, require => Class['apt'] }
-  package { "npm=${npm_version}": ensure => installed, require => Class['apt'] }
+  package { "nodejs=${nodejs_version}":
+    alias   => nodejs,
+    ensure  => installed,
+    require => Class['apt']
+  }
+
+  package { "nodejs-dev=${nodejs_version}":
+    alias   => nodejs-dev,
+    ensure  => installed,
+    require => [ Class['apt'], Package['nodejs'] ]
+  }
+
+  package { "npm=${npm_version}":
+    alias   => npm,
+    ensure  => installed,
+    require => [ Class['apt'], Package['nodejs-dev'] ]
+  }
 
   # Force the npm bundled version of node-gyp to upgrade node-gyp. Needed to build node-expat and hiredis
   exec { 'npm_reinstall_nodegyp':
     command   => "npm install node-gyp@${nodegyp_version}",
     cwd       => '/usr/lib/nodejs/npm',
     logoutput => 'on_failure',
-    require   => Package["npm=${npm_version}"],
+    require   => Package['npm'],
   }
 }
