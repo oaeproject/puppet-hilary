4f10b08b4a8b0857ad1914e35184fa727fcc2586
Branden Visser <mrvisser@gmail.com>
2013-04-15 13:12:31 -0400

    Fix boolean logic for redis slave

--- modules/oaeservice/manifests/redis.pp
+++ modules/oaeservice/manifests/redis.pp
@@ -3,8 +3,8 @@ class oaeservice::redis ($is_slave) {
     # If the service is a slave instance, we configure the master IP in as the slaveof
     $master_host = hieraptr('cache_host_master')
     case $is_slave {
-        false:      { $slave_of = $master_host }
-        default:    { $slave_of = false }
+        false:      { $slave_of = false }
+        default:    { $slave_of = $master_host }
     }
 
     class { '::redis': slave_of => $slave_of }
