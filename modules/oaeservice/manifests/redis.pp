716873e1914ef488d597d8878297489040ffe19a
Branden Visser <mrvisser@gmail.com>
2013-04-15 12:43:33 -0400

    Try removing the default param for redis manifest

--- modules/oaeservice/manifests/redis.pp
+++ modules/oaeservice/manifests/redis.pp
@@ -1,4 +1,4 @@
-class oaeservice::redis ($is_slave = false) {
+class oaeservice::redis ($is_slave) {
 
     # If the service is a slave instance, we configure the master IP in as the slaveof
     $master_host = hiera('cache_host_master')
