a687830d523327033bb62a1192ff4eab637d2b2c
Branden Visser <mrvisser@gmail.com>
2013-04-16 14:30:29 -0400

    Fix custom hiera variable lookup issues

--- modules/oaeservice/lib/puppet/parser/functions/map_hieraptr.rb
+++ modules/oaeservice/lib/puppet/parser/functions/map_hieraptr.rb
@@ -5,6 +5,7 @@ module Puppet::Parser::Functions
     
     hiera_var = arguments[0]
     hiera_default = arguments[1]
+    hiera_val = nil
 
     raise(ArgumentError, 'Must provide an array of hiera variable names') unless hiera_var
 
@@ -12,8 +13,20 @@ module Puppet::Parser::Functions
     Puppet::Parser::Functions.function(:hiera_array)
     Puppet::Parser::Functions.function(:hiera_hash)
 
-    hiera_val = send("function_hiera", [hiera_var, hiera_default])
+    begin
+      # Get the hiera value, then fetch the hiera value with that value
+      hiera_val = send("function_hiera", [hiera_var])
+    rescue
+      # If we barfed getting the value, then we should just use the default if it was specified
+      if (defined?(hiera_default))
+        return hiera_default
+      end
 
+      # No default, raise the error
+      raise
+    end
+
+    # We found the variable successfully, it should be an array, iterate it and push each variable back into hiera
     values = []
     hiera_val.each do |inner_var|
       values.push(send("function_hiera", [inner_var]))
