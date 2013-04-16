fe13a8fbdf683bc6be8a12a6a194546a2bd0b6d8
Branden Visser <mrvisser@gmail.com>
2013-04-16 12:24:20 -0400

    Pass array for custom function

--- modules/oaeservice/lib/puppet/parser/functions/map_hiera.rb
+++ modules/oaeservice/lib/puppet/parser/functions/map_hiera.rb
@@ -1,23 +1,25 @@
 require 'puppet/parser/functions'
 
-Puppet::Parser::Functions.newfunction(:map_hiera, :type => :rvalue) do |arguments|
-  
-  var_names = arguments[0] if arguments[0]
-  hiera_function = arguments[1] if arguments[1]
+module Puppet::Parser::Functions
+  newfunction(:map_hiera, :type => :rvalue) do |arguments|
+    
+    var_names = arguments[0] if arguments[0]
+    hiera_function = arguments[1] if arguments[1]
 
-  raise(ArgumentError, 'Must provide an array of hiera variable names') unless var_names
+    raise(ArgumentError, 'Must provide an array of hiera variable names') unless var_names
 
-  hiera_function ||= 'hiera'
+    hiera_function ||= 'hiera'
 
-  Puppet::Parser::Functions.function(:hiera)
-  Puppet::Parser::Functions.function(:hiera_array)
-  Puppet::Parser::Functions.function(:hiera_hash)
+    Puppet::Parser::Functions.function(:hiera)
+    Puppet::Parser::Functions.function(:hiera_array)
+    Puppet::Parser::Functions.function(:hiera_hash)
 
-  var_values = []
+    var_values = []
 
-  var_names.each do |var_name|
-    var_values.push(send("function_#{hiera_function}", var_name))
+    var_names.each do |var_name|
+      var_values.push(send("function_#{hiera_function}", var_name))
+    end
+
+    return var_values
   end
-
-  return var_values
 end
