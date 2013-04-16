ff0d5e530c1555a69730a27938f4ea22b9245d8b
Branden Visser <mrvisser@gmail.com>
2013-04-16 16:13:18 -0400

    Fix proxy variable scoping issue in haproxy.cfg.erb

--- modules/haproxy/manifests/init.pp
+++ modules/haproxy/manifests/init.pp
@@ -1,11 +1,5 @@
 class haproxy ($version, $template, $enabled = true) {
-    class { '::haproxy::package':
-        version => $version
-    } ->
-    class { '::haproxy::config':
-        template => $template
-    } ->
-    class { '::haproxy::service':
-        enabled => $enabled
-    }
+    class { '::haproxy::package': version => $version } ->
+    class { '::haproxy::config': template => $template } ->
+    class { '::haproxy::service': enabled => $enabled }
 }
