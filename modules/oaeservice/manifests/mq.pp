63e90e1c04288ba3df18b940ef0cb565de4d6fdf
Branden Visser <mrvisser@gmail.com>
2013-04-17 14:55:57 -0400

    Making it possible to configure multiple mq hosts

--- modules/oaeservice/manifests/mq.pp
+++ modules/oaeservice/manifests/mq.pp
@@ -7,13 +7,11 @@ class oaeservice::mq {
   Class['::oaeservice::deps::package::java6']   -> Class['::rabbitmq::server']
   Class['::oaeservice::deps::package::erlang']  -> Class['::rabbitmq::server']
 
-  class { 'rabbitmq::repo::apt':
-    pin    => 900,
-    before => Class['rabbitmq::server']
-  }
+  class { '::rabbitmq::repo::apt': pin => 900 }
 
-  class { 'rabbitmq::server':
-    config_cluster => true,
-    cluster_disk_nodes => map_hieraptr('mq_hosts'),
+  class { '::rabbitmq::server':
+    config_cluster      => true,
+    cluster_disk_nodes  => map_hieraptr('mq_hosts'),
+    require             => Class['::rabbitmq::repo::apt'],
   }
 }
