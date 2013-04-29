d534aff41f23528ff61357520628ba4835d1f15a
Simon Gaeremynck <gaeremyncks@gmail.com>
2013-04-29 11:58:23 +0100

    Removed tmp/shared disk check from oaeservice::nagios::nginx.

--- modules/oaeservice/manifests/nagios/nginx.pp
+++ modules/oaeservice/manifests/nagios/nginx.pp
@@ -10,20 +10,4 @@ class oaeservice::nagios::nginx {
     check_command       => "check_nrpe_1arg!check_nginx_running",
     target              => "/etc/nagios3/conf.d/puppet/services/$hostname-check-nginx-running.cfg",
   }
-
-  @@nagios_service { "${hostname}_check_disk_tmp":
-      use                 => "generic-service",
-      service_description => "Disk::Temp space",
-      host_name           => "$hostname",
-      check_command       => "check_nrpe_1arg!check_disk_tmp",
-      target              => "/etc/nagios3/conf.d/puppet/services/$hostname-check-disk-tmp.cfg",
-  }
-
-  @@nagios_service { "${hostname}_check_disk_shared":
-      use                 => "generic-service",
-      service_description => "Disk::Shared",
-      host_name           => "$hostname",
-      check_command       => "check_nrpe_1arg!check_disk_shared",
-      target              => "/etc/nagios3/conf.d/puppet/services/$hostname-check-disk-shared.cfg",
-  }
 }
