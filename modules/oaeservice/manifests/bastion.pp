750c112c097682786325d23b5848163c1e666c75
Simon Gaeremynck <gaeremyncks@gmail.com>
2013-05-10 09:49:07 +0100

    Removed ssh iptable rule where possible.

--- modules/oaeservice/manifests/bastion.pp
+++ modules/oaeservice/manifests/bastion.pp
@@ -10,13 +10,4 @@ class oaeservice::bastion {
     command => 'sysctl -p /etc/sysctl.conf',
     refreshonly => true,
   }
-
-  # Accept SSH traffic on the public interface
-  iptables { '001 allow public ssh traffic':
-    chain   => 'INPUT',
-    iniface => 'eth0',
-    proto   => 'tcp',
-    dport   => 22,
-    jump    => 'ACCEPT',
-  }
 }
--- modules/oaeservice/manifests/nginx.pp
+++ modules/oaeservice/manifests/nginx.pp
@@ -16,13 +16,4 @@ class oaeservice::nginx {
     ux_root_dir                     => hiera('ux_root_dir'),
     files_home                      => hiera('app_files_dir')
   }
-
-  # Accept SSH traffic on the public interface
-  iptables { '001 allow public ssh traffic':
-    chain   => 'INPUT',
-    iniface => 'eth0',
-    proto   => 'tcp',
-    dport   => 22,
-    jump    => 'ACCEPT',
-  }
 }
