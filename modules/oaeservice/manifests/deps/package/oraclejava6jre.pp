889a08a4ccc6b4e5611be4964f6e069197773d52
Branden Visser <mrvisser@gmail.com>
2013-06-11 09:44:58 -0400

    Missing path

--- modules/oaeservice/manifests/deps/package/oraclejava6jre.pp
+++ modules/oaeservice/manifests/deps/package/oraclejava6jre.pp
@@ -10,7 +10,8 @@ class oaeservice::deps::package::oraclejava6jre {
     }
 
     file { 'oab-java.sh':
-        mode => 0744,
+        path    => "$script_dir/oab-java.sh"
+        mode    => 0744,
         require => Archive::Download['oab-java.sh'],
     }
 
