cfe42032af1d6f27bc247de80c8fcaa762c1fbec
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2013-06-24 11:03:02 -0400

    Re-introducing @stuartf's oracle java install module

--- modules/oaeservice/manifests/deps/package/oraclejava6jre.pp
+++ modules/oaeservice/manifests/deps/package/oraclejava6jre.pp
@@ -1,36 +1,3 @@
-class oaeservice::deps::package::oraclejava6jre ($java_path = '/usr/lib/jvm/java-6-sun/jre/bin/java') {
-    
-    $script_dir = '/opt'
-
-    # Use the Archive module to download and md5 the script for convenience
-    archive::download { 'oab-java.sh':
-        url             => 'https://raw.github.com/flexiondotorg/oab-java6/0.2.8/oab-java.sh',
-        src_target      => $script_dir,
-        digest_string   => 'b145cf455f3dd1ea116d81088ca5620e',
-    }
-
-    file { 'oab-java.sh':
-        path    => "$script_dir/oab-java.sh",
-        mode    => 0744,
-        require => Archive::Download['oab-java.sh'],
-    }
-
-    # Allow 10min to download and install java
-    exec { 'oab-java.sh':
-        command     => "$script_dir/oab-java.sh",
-        timeout     => 600,
-        logoutput   => 'on_failure',
-        creates     => $java_path,
-        require     => File['oab-java.sh'],
-    }
-
-    package { 'sun-java6-jre':
-        ensure  => installed,
-        require => Exec['oab-java.sh'],
-    }
-
-    alternatives { 'java':
-        path => $java_path,
-        require => Package['sun-java6-jre']
-    }
+class oaeservice::deps::package::oraclejava6jre {
+    include ::oracle-java
 }
---
+++ modules/oracle-java/manifests/init.pp
@@ -0,0 +1,30 @@
+class oracle-java (
+        $file_name    = 'jdk-6u45-linux-x64.bin',
+        $package_name = 'jdk1.6.0_45'
+    ) {
+
+    file { '/usr/lib/jvm/':
+        ensure => directory,
+    }
+
+    file { "/usr/lib/jvm/${file_name}":
+        ensure  => present,
+        mode    => 700,
+        source  => "puppet:///modules/oracle-java/${file_name}",
+        require => File['/usr/lib/jvm/'],
+        notify  => Exec['unpack-java'],
+    }
+
+    exec { 'unpack-java':
+        command     => $file_name,
+        cwd         => '/usr/lib/jvm/',
+        path        => '/usr/lib/jvm/',
+        refreshonly => true,
+        notify      => Alternatives['java'],
+    }
+
+    alternatives { 'java':
+        path => $java_path,
+        require => Package['sun-java6-jre']
+    }
+}
