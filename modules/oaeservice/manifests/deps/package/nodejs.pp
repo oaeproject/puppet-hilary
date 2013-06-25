d38421e5e4e5a7e13009ea266ef3af635757cf9d
Branden Visser <mrvisser@gmail.com>
2013-06-25 12:17:34 -0400

    Change npm install resource name; Update performance nfs ip

--- modules/oaeservice/manifests/deps/package/nodejs.pp
+++ modules/oaeservice/manifests/deps/package/nodejs.pp
@@ -19,7 +19,7 @@ class oaeservice::deps::package::nodejs ($nodejs_version, $npm_version, $nodegyp
 
   if $npm_version == '1.3.0-1chl1~precise1' {
     ## Workaround to fix issue with semver 2.0.7 where "nmax" semver went to the min, not the max :(
-    exec { 'npm_reinstall_nodegyp':
+    exec { 'npm_upgrade_semver':
       command   => "npm install semver@2.0.8",
       cwd       => '/usr/lib/nodejs/npm',
       logoutput => 'on_failure',
