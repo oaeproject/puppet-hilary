d225fc2fc68f735ff2a09fde2f15d02ab82f6fac
Branden Visser <mrvisser@gmail.com>
2013-06-25 11:24:51 -0400

    Upgrade semver to 2.0.8 since 2.0.7 has a crazy bug with nmax really being nmin

--- modules/oaeservice/manifests/deps/package/nodejs.pp
+++ modules/oaeservice/manifests/deps/package/nodejs.pp
@@ -17,6 +17,16 @@ class oaeservice::deps::package::nodejs ($nodejs_version, $npm_version, $nodegyp
     require => [ Class['apt'], Package['nodejs-dev'] ]
   }
 
+  if $npm_version == '1.3.0-1chl1~precise1' {
+    ## Workaround to fix issue with semver 2.0.7 where "nmax" semver went to the min, not the max :(
+    exec { 'npm_reinstall_nodegyp':
+      command   => "npm install semver@2.0.8",
+      cwd       => '/usr/lib/nodejs/npm',
+      logoutput => 'on_failure',
+      require   => Package['npm'],
+    }
+  }
+
   # Force the npm bundled version of node-gyp to upgrade node-gyp. Needed to build node-expat and hiredis
   exec { 'npm_reinstall_nodegyp':
     command   => "npm install node-gyp@${nodegyp_version}",
