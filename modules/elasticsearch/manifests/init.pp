3395f18cb1bdf5b43d3e4814c3ab8d94f53f11ab
Branden Visser <mrvisser@gmail.com>
2013-06-27 12:27:59 -0400

    Miscellaneous search profiling fixes

--- modules/elasticsearch/manifests/init.pp
+++ modules/elasticsearch/manifests/init.pp
@@ -6,6 +6,7 @@ class elasticsearch (
     $checksum,
     $host_port        = 9200,
     $heap_size_mb     = 384,
+    $heap_newsize_mb  = null,
     $rsyslog_enabled  = false,
     $rsyslog_host     = '127.0.0.1') {
 
