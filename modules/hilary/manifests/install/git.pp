ef5ad67641409bd511b2dc4b6ecfbe1d637e03b2
Branden Visser <mrvisser@gmail.com>
2013-07-06 09:48:57 -0400

    Fix bad merge, missing HOME env var on hilary npm install

--- modules/hilary/manifests/install/git.pp
+++ modules/hilary/manifests/install/git.pp
@@ -25,7 +25,7 @@ class hilary::install::git (
         cwd         => $app_root_dir,
 
         # Forcing CFLAGS for std=c99 for hiredis, until https://github.com/pietern/hiredis-node/pull/33 is resolved
-        environment => 'CFLAGS="-std=c99"',
+        environment => ['CFLAGS="-std=c99"', 'HOME=/root'],
         command     => 'npm install -d',
         logoutput   => 'on_failure',
 
