7badcd6dfb4738d55583383d7916660fbad9823c
Branden Visser <mrvisser@gmail.com>
2013-08-08 11:26:15 -0400

    Fixing tsung install

--- modules/tsung/manifests/install/archive.pp
+++ modules/tsung/manifests/install/archive.pp
@@ -6,15 +6,17 @@ class tsung::install::archive ($target_dir = '/opt/tsung', $version = '1.5.0') {
         checksum    => false,
     }
 
+    $tsung_src = "/usr/src/tsung-${version}"
+
     exec { 'tsung_configure':
-        cwd         => '/usr/src/tsung',
-        command     => 'configure --prefix=/opt/tsung',
+        cwd         => $tsung_src,
+        command     => './configure --prefix=/opt/tsung',
         unless      => 'test -d /opt/tsung',
         require     => Archive['tsung'],
     }
 
     exec { 'tsung_make_install':
-        cwd         => '/usr/src/tsung',
+        cwd         => $tsung_src,
         command     => 'make && make install',
         creates     => '/opt/tsung',
         require     => Exec['tsung_configure'],
