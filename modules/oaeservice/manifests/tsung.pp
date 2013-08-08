c2e26a8bc0c458f00dbf55b9d885c4d65f246290
Branden Visser <mrvisser@gmail.com>
2013-08-08 11:43:58 -0400

    Adding git repos

--- modules/oaeservice/manifests/tsung.pp
+++ modules/oaeservice/manifests/tsung.pp
@@ -3,5 +3,20 @@ class oaeservice::tsung {
     include ::oaeservice::deps::package::gnuplot
     include ::tsung::install::archive
 
-    Class['::oaeservice::deps::package::erlang'] -> Class['::tsung::install::archive']
+    Class['::oaeservice::deps::package::erlang']    -> Class['::tsung::install::archive']
+    Class['::oaeservice::deps::package::git']       -> Vcsrepo<| |>
+
+    vcsrepo { '/opt/node-oae-tsung':
+        ensure      => latest,
+        provider    => git,
+        source      => 'https://github.com/oaeproject/node-oae-tsung',
+        revision    => 'master',
+    }
+
+    vcsrepo { '/opt/OAE-model-loader':
+        ensure      => latest,
+        provider    => git,
+        source      => 'https://github.com/oaeproject/OAE-model-loader',
+        revision    => 'master',
+    }
 }
