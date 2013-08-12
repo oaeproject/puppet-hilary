c80a57dcd4225f95cb0314f91741a533ab2ba022
Branden Visser <mrvisser@gmail.com>
2013-08-12 13:32:39 -0400

    Install nginx on driver

--- modules/oaeservice/manifests/driver.pp
+++ modules/oaeservice/manifests/driver.pp
@@ -1,14 +1,46 @@
 class oaeservice::driver {
-  class { 'tsung': }
+    include ::oaeservice::deps::common
+    include ::oaeservice::deps::package::erlang
+    include ::oaeservice::deps::package::gnuplot
+    include ::oaeservice::deps::package::graphicsmagick
+    include ::oaeservice::deps::package::nodejs
+    include ::tsung::install::archive
 
-  package { 'nginx':
-    ensure    => present,
-    provider  => pkgin,
-  }
+    Class['::oaeservice::deps::package::erlang']    -> Class['::tsung::install::archive']
+    Class['::oaeservice::deps::package::git']       -> Vcsrepo<| |>
 
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
-  }
+
+    # Install OAE-model-loader and node-oae-tsung
+
+    $oae_tsung_dir = '/opt/node-oae-tsung'
+    $oae_loader_dir = '/opt/OAE-model-loader'
+
+    vcsrepo { $oae_tsung_dir:
+        ensure      => latest,
+        provider    => git,
+        source      => 'https://github.com/oaeproject/node-oae-tsung',
+        revision    => 'master',
+    }
+
+    exec { 'npm_install_tsung':
+        cwd     => $oae_tsung_dir,
+        command => 'npm install -d',
+        require => Vcsrepo[$oae_tsung_dir]
+    }
+
+    vcsrepo { $oae_loader_dir:
+        ensure      => latest,
+        provider    => git,
+        source      => 'https://github.com/oaeproject/OAE-model-loader',
+        revision    => 'master',
+    }
+
+    exec { 'npm_install_modelloader':
+        cwd     => $oae_loader_dir,
+        command => 'npm install -d',
+        require => Vcsrepo[$oae_loader_dir]
+    }
+
+    # Install Nginx, so we have a place to put the data
+    package { 'nginx': }
 }
