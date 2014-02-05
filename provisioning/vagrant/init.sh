c211bd3df0ec97c83e80bb39768fff98d8098627
Simon Gaeremynck <gaeremyncks@gmail.com>
2014-02-05 13:01:22 +0000

    Fixed up the Vagrant config

--- provisioning/vagrant/init.sh
+++ provisioning/vagrant/init.sh
@@ -18,10 +18,11 @@ if [ ! -x /vagrant/modules/oracle-java/files/$ORACLE_JDK_INSTALLER ] ; then
     chmod 755 /vagrant/modules/oracle-java/files/$ORACLE_JDK_INSTALLER
 fi
 
-# We need puppet version 3.3.1
+
+# We need puppet version 3+
 PUPPET_VERSION=$(puppet help | tail -n 1 | cut -f 2 -d " ")
-if [ "${PUPPET_VERSION}" != "v3.3.1" ] ; then
-    echo "Puppet was on version ${PUPPET_VERSION} but should be on version 3.3.1. Updating."
+if [ "${PUPPET_VERSION}" == "v2.7.19" ] ; then
+    echo "Puppet was on version ${PUPPET_VERSION} but should be on version 3+ Updating."
 
     echo "Installing puppetlabs repo"
     cd /tmp
@@ -30,7 +31,7 @@ if [ "${PUPPET_VERSION}" != "v3.3.1" ] ; then
     apt-get update
 
     echo "Updating puppet"
-    apt-get -y install puppet=3.3.1-1puppetlabs1
+    apt-get -y install puppet
 fi
 
 # We need puppet/apt
@@ -63,15 +64,25 @@ apt-get update
 cd /vagrant
 sh bin/pull.sh
 
+# Create the hiera config
+cat > /etc/puppet/hiera.yaml <<EOF
+:backends:
+  - json
+:json:
+  :datadir: /vagrant/environments/%{::environment}/hiera
+:hierarchy:
+  - common
+EOF
+
 # Run puppet
 echo "Applying puppet catalog. This might take a while (~30+ mins is not unreasonable)"
-puppet apply --verbose --debug --modulepath environments/local/modules:modules:/etc/puppet/modules --certname dev --environment local --hiera_config provisioning/vagrant/hiera.yaml site.pp
+puppet apply --verbose --debug --modulepath environments/local/modules:modules:/etc/puppet/modules --certname dev --environment local site.pp
 
 STATUS_CODE=$?
 if [ $STATUS_CODE -ne 0 ] ; then
     echo "Got a ${STATUS_CODE} status code, which indicates the puppet catalog could not be properly applied."
     echo "There are a couple of possible things you can do:"
-    echo " - Run vagrant ssh and try running cd /vagrant && puppet apply --verbose --debug --modulepath environments/local/modules:modules:/etc/puppet/modules --certname dev --environment local --hiera_config provisioning/vagrant/hiera.yaml site.pp"
+    echo " - Run vagrant ssh and try running cd /vagrant && puppet apply --verbose --debug --modulepath environments/local/modules:modules:/etc/puppet/modules --certname dev --environment local site.pp"
     echo " - If you're familiar with puppet try to analyze the output and tweak the puppet scripts"
     echo " - Hop onto #sakai on irc.freenode.org and ask if anyone has seen your error"
     echo " - Shoot an e-mail to oae-dev@sakaiproject.org with the above output"
@@ -85,8 +96,13 @@ service cassandra restart
 service elasticsearch restart
 service rabbitmq-server restart
 service redis-server restart
-service hilary restart
 service nginx restart
+
+echo "All the dependencies have been restarted, sleeping for a bit to give them time to start up properly"
+sleep 5
+
+# Start the app server
+service hilary restart
 
 echo "Sleeping 15 seconds to give the app server a little bit of time to start up"
 sleep 15
