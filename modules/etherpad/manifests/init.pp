bd62139a9c160664548c1b936e99a45221c8a866
Branden Visser <mrvisser@gmail.com>
2014-03-28 08:54:19 -0400

    Add archive etherpad install, remove git and apt

--- modules/etherpad/manifests/init.pp
+++ modules/etherpad/manifests/init.pp
@@ -8,41 +8,24 @@ class etherpad (
         $oae_db_replication,
         $oae_db_strategy_class,
 
-        $install_method         = 'apt',
-        $apt_package_version    = '1.2.91-4',
+        $install_method         = 'archive',
+        $install_config         = {
+            'url_base'              => 'https://s3.amazonaws.com/oae-releases/etherpad',
+            'version_major_minor'   => '1.2',
+            'version_patch'         => '91',
+            'version_nodejs'        => '0.10.17',
+        },
+
         $etherpad_dir           = '/opt/etherpad',
-        $etherpad_git_source    = 'https://github.com/ether/etherpad-lite',
-        $etherpad_git_revision  = '1.2.91',
-        $ep_oae_git_source      = 'https://github.com/oaeproject/ep_oae',
-        $ep_oae_git_revision    = 'master',
         $etherpad_user          = 'etherpad',
         $service_name           = 'etherpad',
         $enable_abiword         = false) {
 
-    # Install etherpad.
-    case $install_method {
-        'git': {
-            class { '::etherpad::install::git':
-                etherpad_dir            => $etherpad_dir,
-                etherpad_git_source     => $etherpad_git_source,
-                etherpad_git_revision   => $etherpad_git_revision,
-                ep_oae_git_source       => $ep_oae_git_source,
-                ep_oae_git_revision     => $ep_oae_git_revision,
-            }
-        }
-        'apt': {
-            class { '::etherpad::install::apt':
-                package_version => $apt_package_version
-            }
-        }
-        default: {
-            fail("Unknown install method for etherpad passed in: '${install_method}'")
-        }
+    # Install etherpad
+    class { "::etherpad::install::${install_method}":
+        install_config      => $install_config,
+        etherpad_root_dir   => $etherpad_dir,
     }
-
-    Class["::etherpad::install::${install_method}"] -> File['etherpad_settings_json']
-    Class["::etherpad::install::${install_method}"] -> File['etherpad_apikey_txt']
-    Class["::etherpad::install::${install_method}"] -> File['etherpad_service']
 
     if ($enable_abiword) {
         package { 'abiword':
@@ -50,37 +33,54 @@ class etherpad (
         }
     }
 
-
     user { "${etherpad_user}": ensure => present }
+
+    # Install the custom CSS for etherpad from the ep_oae plugin. This is being put in
+    # both the src/ and ep_etherpad-lite/ because it doesn't always work for some reason
+    # when put in src/
+    file { "${etherpad_dir}/node_modules/ep_etherpad-lite/static/custom/pad.css":
+        ensure     => present,
+        source     => "${etherpad_dir}/node_modules/ep_oae/static/css/pad.css",
+        require    => Class["::etherpad::install::${install_method}"],
+    }
+
+    file { "${etherpad_dir}/src/static/custom/pad.css":
+        ensure     => present,
+        source     => "${etherpad_dir}/node_modules/ep_oae/static/css/pad.css",
+        require    => Class["::etherpad::install::${install_method}"],
+    }
 
     # Apply our custom settings.json file
     file { 'etherpad_settings_json':
-        path        =>  "${etherpad_dir}/settings.json",
-        ensure      =>  present,
-        mode        =>  '0644',
-        content     =>  template('etherpad/etherpad.settings.json.erb'),
-        require     => Class["::etherpad::install::${install_method}"]
+        path    => "${etherpad_dir}/settings.json",
+        ensure  => present,
+        mode    => '0644',
+        content => template('etherpad/etherpad.settings.json.erb'),
+        require => Class["::etherpad::install::${install_method}"],
     }
 
-    # The file that will contain the shared secret.
+    # The file that will contain the shared secret
     file { 'etherpad_apikey_txt':
-        path        => "${etherpad_dir}/APIKEY.txt",
-        ensure      =>  present,
-        content     =>  $api_key,
-        mode        =>  '0644',
+        path    => "${etherpad_dir}/APIKEY.txt",
+        ensure  => present,
+        content => $api_key,
+        mode    => '0644',
+        require => Class["::etherpad::install::${install_method}"],
     }
 
-    exec { "chown_etherpad_dir":
-        command    => "chown -R ${etherpad_user}:${etherpad_user} ${etherpad_dir}",
-        cwd        => $etherpad_dir,
-        require    => [ File['etherpad_apikey_txt'], User[$etherpad_user] ]
+    # Ensure the etherpad user owns all the etherpad resources
+    exec { 'chown_etherpad_dir':
+        command => "chown -R ${etherpad_user}:${etherpad_user} ${etherpad_dir}",
+        cwd     => $etherpad_dir,
+        require => [File['etherpad_apikey_txt'], User[$etherpad_user]],
     }
 
     # Daemon script for the etherpad service
     file { 'etherpad_service':
         path    => "/etc/init/${service_name}.conf",
-        ensure  =>  present,
-        content =>  template('etherpad/upstart_etherpad.conf.erb'),
+        ensure  => present,
+        content => template('etherpad/upstart_etherpad.conf.erb'),
+        require => [Class["::etherpad::install::${install_method}"]],
     }
 
     # Start the etherpad server
@@ -88,8 +88,12 @@ class etherpad (
         ensure      => running,
         provider    => upstart,
         require     => [
+            File["${etherpad_dir}/node_modules/ep_etherpad-lite/static/custom/pad.css"],
+            File["${etherpad_dir}/src/static/custom/pad.css"],
+            File['etherpad_settings_json'],
+            File['etherpad_apikey_txt'],
             Exec['chown_etherpad_dir'],
-            File['etherpad_service']
-        ]
+            File['etherpad_service'],
+        ],
     }
 }
---
+++ modules/etherpad/manifests/install/archive.pp
@@ -0,0 +1,59 @@
+# == Class: etherpad::install::archive
+#
+# This class is responsible for installing the necessary etherpad files from a remote archive. It expects that there will be
+# both a package and a checksum available at a remote URL.
+#
+# The checksum URL should be located at <package url including extension>.<checksum_type>.txt
+#
+# === Parameters
+#
+# [*install_config['url_base']*]
+#   The part of the archive source URI that would come just before the minor version directory (e.g., https://s3.amazonaws.com/oae-releases/etherpad)
+#
+# [*install_config['url_extension']*]
+#   The extension of the archive, without the filename of the parent directory URI (e.g., tar.gz). Defaults to 'tar.gz'
+#
+# [*install_config['version_major_minor']*]
+#   The major and minor version of the package in the format: "<major>.<minor>" (e.g., 1.2)
+#   The filename portion of the archive without the parent directory URI or the extension (e.g., etherpad-1.2.91_node-0.10.15)
+#
+# [*install_config['version_patch']*]
+#   The patch version of the package (e.g., 0 or 2-<# of commits since tag>-<commit hash>)
+#
+# [*install_config['version_nodejs']*]
+#   The nodejs version with which this package was prepared (e.g., 0.10.15)
+#
+# [*install_config['checksum_type']*]
+#   The type of checksum (e.g., sha1, md5). Defaults to 'sha1'
+#
+# [*etherpad_root_dir*]
+#   The target directory to extract the release archive. Defaults to '/opt/etherpad'
+#
+class etherpad::install::archive ($install_config, $etherpad_root_dir = '/opt/etherpad') {
+
+    $_install_config = merge({
+        'url_extension' => 'tar.gz',
+        'checksum_type' => 'sha1'
+    }, $install_config)
+
+    $url_base               = $_install_config['url_base']
+    $url_extension          = $_install_config['url_extension']
+    $version_major_minor    = $_install_config['version_major_minor']
+    $version_patch          = $_install_config['version_patch']
+    $version_nodejs         = $_install_config['version_nodejs']
+    $checksum_type          = $_install_config['checksum_type']
+
+    $url_filename = "etherpad-${version_major_minor}.${version_patch}_node-${version_nodejs}"
+    $package_url = "${url_base}/${version_major_minor}/${url_filename}.${url_extension}"
+    $checksum_url = "${package_url}.${checksum_type}.txt"
+
+    # Download and unpack the archive
+    archive { "${url_filename}":
+        ensure          => present,
+        url             => $package_url,
+        target          => $etherpad_root_dir,
+        digest_url      => $checksum_url,
+        digest_type     => $checksum_type,
+        extension       => $url_extension,
+    }
+}
