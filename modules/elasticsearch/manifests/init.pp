439d198c8a669b9c45746c5866bed369eaccd36d
Simon Gaeremynck <gaeremyncks@gmail.com>
2014-05-12 15:38:46 +0100

    Cleaned up elasticsearch module and brought in 1.1.1 config

--- modules/elasticsearch/manifests/init.pp
+++ modules/elasticsearch/manifests/init.pp
@@ -9,44 +9,43 @@ class elasticsearch (
     $rsyslog_enabled  = false,
     $rsyslog_host     = '127.0.0.1') {
 
-  $filename = "elasticsearch-${version}.deb"
+    ########################################
+    ## DOWNLOAD AND COMPILE ELASTICSEARCH ##
+    ########################################
 
-  ########################################
-  ## DOWNLOAD AND COMPILE ELASTICSEARCH ##
-  ########################################
+    # Ensure the data directory exists
+    exec { 'mkdir_data':
+        command   => "mkdir -p ${path_data}",
+        unless    => "test -d ${path_data}",
+    }
 
-  # Ensure the data directory exists
-  exec { 'mkdir_data':
-    command   => "mkdir -p ${path_data}",
-    unless    => "test -d ${path_data}",
-  }
+    package { 'elasticsearch':
+        ensure => $version
+    }
 
-  package { 'elasticsearch': ensure => $version }
+    file { '/etc/init.d/elasticsearch':
+        ensure  => present,
+        mode    => '0755',
+        content => template('elasticsearch/elasticsearch.erb'),
+        require => Package['elasticsearch']
+    }
 
-  file { '/etc/init.d/elasticsearch':
-    ensure  => present,
-    mode    => '0755',
-    content => template('elasticsearch/elasticsearch.erb'),
-    require => Package['elasticsearch']
-  }
+    file { '/etc/elasticsearch/elasticsearch.yml':
+        ensure  => present,
+        content => template('elasticsearch/elasticsearch.yml.erb'),
+        require => Package['elasticsearch'],
+    }
 
-  file { '/etc/elasticsearch/elasticsearch.yml':
-    ensure  => present,
-    content => template('elasticsearch/elasticsearch.yml.erb'),
-    require => Package['elasticsearch'],
-  }
+    file { '/etc/elasticsearch/logging.yml':
+        ensure  => present,
+        content => template('elasticsearch/logging.yml.erb'),
+        require => Package['elasticsearch'],
+    }
 
-  file { '/etc/elasticsearch/logging.yml':
-    ensure  => present,
-    content => template('elasticsearch/logging.yml.erb'),
-    require => Package['elasticsearch'],
-  }
-
-  service { 'elasticsearch':
-    ensure  => 'running',
-    enable  => true,
-    require => [ Package['elasticsearch'], File['/etc/elasticsearch/elasticsearch.yml'],
-      File['/etc/elasticsearch/logging.yml'], File['/etc/init.d/elasticsearch'], Exec['mkdir_data'] ],
-  }
-
+    service { 'elasticsearch':
+        ensure  => 'running',
+        enable  => true,
+        require => [ Package['elasticsearch'], File['/etc/elasticsearch/elasticsearch.yml'],
+            File['/etc/elasticsearch/logging.yml'], File['/etc/init.d/elasticsearch'], Exec['mkdir_data'] ],
+    }
 }
