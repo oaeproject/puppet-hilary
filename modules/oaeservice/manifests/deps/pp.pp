8413aedd64d37d3891f8a1ad470aeddd2f7cd879
Simon Gaeremynck <gaeremyncks@gmail.com>
2014-08-15 10:21:35 +0100

    Libreoffice 4.3 + fonts

--- modules/oaeservice/manifests/deps/pp.pp
+++ modules/oaeservice/manifests/deps/pp.pp
@@ -1,17 +1,46 @@
 class oaeservice::deps::pp {
-  include oaeservice::deps::common
-  include oaeservice::deps::ppa::libreoffice
-  include oaeservice::deps::ppa::oae
-  include oaeservice::deps::ppa::pdf2htmlex
+    include oaeservice::deps::common
+    include oaeservice::deps::ppa::libreoffice
+    include oaeservice::deps::ppa::oae
+    include oaeservice::deps::ppa::pdf2htmlex
 
-  # Necessary packages for the preview processor
-  $pp_packages = [
-    'libreoffice',
-    'pdftk',
-    'chrpath',
-    'libfontconfig1-dev',
-    'fonts-international',
-    'pdf2htmlex'
-  ]
-  package { $pp_packages: ensure => installed }
+    # Necessary packages for the preview processor
+    $pp_packages = [
+        'pdftk',
+        'chrpath',
+        'libfontconfig1-dev',
+        'ttf-liberation',
+        'texlive-fonts-recommended',
+        'fonts-international',
+        'pdf2htmlex'
+    ]
+    package { $pp_packages:
+        ensure   => installed
+    }
+
+    # Install the correct version of libreoffice
+    package {'libreoffice':
+        ensure   => '1:4.3.0-0ubuntu1~precise1'
+    }
+
+    # Install some more fonts because libreoffice 4.3
+    # doesn't seem to autoguess any longer
+    exec { 'accept-msttcorefonts-license':
+        command => '/bin/sh -c "echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections"'
+    }
+    package { 'ttf-mscorefonts-installer':
+        ensure   => installed,
+        require  => Exec['accept-msttcorefonts-license']
+    }
+
+    # Install more msfonts
+    file { '/tmp/fonts.sh':
+        content  => template('oaeservice/msfonts/fonts.sh'),
+        mode     => "0755",
+    }
+    # Only run if there is a font missing
+    exec {'/tmp/fonts.sh':
+        creates  => '/usr/share/fonts/truetype/msttcorefonts/MTCORSVA.TTF',
+        require  => [ File['/tmp/fonts.sh'], Package['ttf-mscorefonts-installer'] ]
+    }
 }
