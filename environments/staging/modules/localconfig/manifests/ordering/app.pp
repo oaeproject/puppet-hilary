a58d200a3b37a26217a6f8b850198609edd3191f
David Stringer <David.Stringer@researchresearch.com>
2015-02-27 11:42:39 +0000

    removed SAMLParser

--- environments/staging/modules/localconfig/manifests/ordering/app.pp
+++ environments/staging/modules/localconfig/manifests/ordering/app.pp
@@ -2,5 +2,4 @@ class localconfig::ordering::app {
   # Get the files mounted before we tackle the app server
   Class['::oaeservice::hilary::files']              -> Class['::hilary']
   Class['::oaeservice::deps::package::openjdk6']    -> Class['::hilary']
-  Class['::oaeservice::deps::package::samlparser']  -> Class['::hilary']
 }
