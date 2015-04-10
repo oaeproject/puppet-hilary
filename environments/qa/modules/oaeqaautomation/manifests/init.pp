3ae170528115d90de3a06392ab0e52cfd417693c
David Stringer <David.Stringer@researchresearch.com>
2015-04-10 12:39:19 +0100

    added lock file for redeploy cronjob and other small tweaks

--- environments/qa/modules/oaeqaautomation/manifests/init.pp
+++ environments/qa/modules/oaeqaautomation/manifests/init.pp
@@ -79,7 +79,7 @@ class oaeqaautomation (
     $cron_log_file_path = "${log_file_dir}/`date +'\\%Y-\\%m-\\%d-\\%H-\\%M'`.log"
     cron { 'nightly-redeploy':
         ensure      => present,
-        command     => "${scripts_dir}/redeploy.sh >> ${cron_log_file_path} 2>> ${cron_log_file_path}",
+        command     => "test -e /var/lock/redeploy || ${scripts_dir}/redeploy.sh >> ${cron_log_file_path} 2>> ${cron_log_file_path}",
         user        => 'root',
         target      => 'root',
         hour        => $cron_hour,
