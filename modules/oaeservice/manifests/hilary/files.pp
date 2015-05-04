f8dcf0355fc83b23109e4e2c75a97d84d66e819b
Branden Visser <mrvisser@gmail.com>
2015-05-04 13:09:11 -0400

    Switch userfiles location configuration to use the temporary migration NFS

--- modules/oaeservice/manifests/hilary/files.pp
+++ modules/oaeservice/manifests/hilary/files.pp
@@ -2,11 +2,11 @@ class oaeservice::hilary::files {
   $user_files_nfs = hiera('app_files_nfs')
   $user_files_dir_parent = hiera('app_files_parent')
 
-  require nfs::client
-  nfs::mount { $user_files_dir_parent:
-    ensure      => present,
-    mountpoint  => $user_files_dir_parent,
-    server      => $user_files_nfs['server'],
-    share       => $user_files_nfs['source_dir'],
-  }
+  #require nfs::client
+  #nfs::mount { $user_files_dir_parent:
+  #  ensure      => present,
+  #  mountpoint  => $user_files_dir_parent,
+  #  server      => $user_files_nfs['server'],
+  #  share       => $user_files_nfs['source_dir'],
+  #}
 }
