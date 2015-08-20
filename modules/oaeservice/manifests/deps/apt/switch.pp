62ffb6c5502dddaab7bafa31751a130e11004757
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2015-08-20 10:42:14 -0400

    use full 40 char fingerprints for gpg keys

--- modules/oaeservice/manifests/deps/apt/switch.pp
+++ modules/oaeservice/manifests/deps/apt/switch.pp
@@ -1,13 +1,13 @@
 class oaeservice::deps::apt::switch {
     ::apt::key { 'switch.ch':
-        key         => '15B76742',
+        key         => '294E37D154156E00FB96D7AA26C3C46915B76742',
         key_source  => 'http://pkg.switch.ch/switchaai/SWITCHaai-swdistrib.asc',
     }
     ::apt::source { 'switch.ch':
         location    => 'http://pkg.switch.ch/switchaai/ubuntu',
         release     => 'precise',
         repos       => 'main',
-        key         => '15B76742',
+        key         => '294E37D154156E00FB96D7AA26C3C46915B76742',
 
         # Switch doesn't publish their sources into the apt repository, don't
         # include them or you will get errors when trying to run `apt-get update`
