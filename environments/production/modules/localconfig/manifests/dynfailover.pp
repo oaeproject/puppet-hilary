4c32f165e944084771fe6688178e6bcf9037d290
David Stringer <David.Stringer@researchresearch.com>
2016-04-14 17:15:01 +0100

    fixed mismatched brackets in manifest

--- environments/production/modules/localconfig/manifests/dynfailover.pp
+++ environments/production/modules/localconfig/manifests/dynfailover.pp
@@ -6,13 +6,13 @@ class dynfailover {
     $web0ip = hiera('web0ip')
     $web1ip = hiera('web1ip')
 
-    file ( 'dyn_failover':
+    file { 'dyn_failover':
         path    => "/usr/local/bin/dyn_failover",
         mode    => 0755,
         content => template('localconfig/dyn_failover.erb')
     }
 
-    cron ( 'dynfailover':
+    cron { 'dynfailover':
         ensure  => present,
         command => "/usr/local/bin/dyn_failover",
         user    => 'root',
