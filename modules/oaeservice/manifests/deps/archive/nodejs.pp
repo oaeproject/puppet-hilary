cf17e11aba68f23d469ef0acbe6d1fc0bdab2f1f
David Stringer <David.Stringer@researchresearch.com>
2017-06-21 16:33:28 +0100

    first of many fixes

--- modules/oaeservice/manifests/deps/archive/nodejs.pp
+++ modules/oaeservice/manifests/deps/archive/nodejs.pp
@@ -5,8 +5,7 @@ class oaeservice::deps::archive::nodejs {
         ensure          => present,
         url             => 'https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.gz',
         target          => '/usr/local/node-v6.10.0',
-#        digest_url      => $checksum_url,
-#        digest_type     => $checksum_type,
+        checksum        => false,
         extension       => 'tar.gz',
     }
 
