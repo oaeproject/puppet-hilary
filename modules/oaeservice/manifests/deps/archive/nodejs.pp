65537654d0a22508aee68630f0a6e0e2bb52dc50
David Stringer <David.Stringer@researchresearch.com>
2017-06-21 16:35:50 +0100

    node path fix

--- modules/oaeservice/manifests/deps/archive/nodejs.pp
+++ modules/oaeservice/manifests/deps/archive/nodejs.pp
@@ -4,7 +4,7 @@ class oaeservice::deps::archive::nodejs {
     archive { "node-v6.10.0":
         ensure          => present,
         url             => 'https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.gz',
-        target          => '/usr/local/node-v6.10.0',
+        target          => '/usr/local',
         checksum        => false,
         extension       => 'tar.gz',
     }
