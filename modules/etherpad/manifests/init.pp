6704de76511b58ac733e5f83959fb225ead9f22e
David Stringer <David.Stringer@researchresearch.com>
2017-06-26 17:30:01 +0100

    make EP run a different version than it was packaged for

--- modules/etherpad/manifests/init.pp
+++ modules/etherpad/manifests/init.pp
@@ -23,6 +23,8 @@ class etherpad (
         $service_name           = 'etherpad',
         $enable_abiword         = false) {
 
+    $global_node_version = hiera('global_nodejs_version')
+
     # Install etherpad
     class { "::etherpad::install::${install_method}":
         install_config      => $install_config,
