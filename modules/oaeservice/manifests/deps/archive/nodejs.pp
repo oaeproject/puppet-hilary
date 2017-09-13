ca4ae74ff248bb92cd4f959e263d135b2f66aeab
Salla Karppinen <sk@researchresearch.com>
2017-09-13 17:19:26 +0100

    Nodejs updates

--- modules/oaeservice/manifests/deps/archive/nodejs.pp
+++ modules/oaeservice/manifests/deps/archive/nodejs.pp
@@ -1,7 +1,7 @@
 class oaeservice::deps::archive::nodejs {
 
     if ! $nodejs_version {
-      $nodejs_version = hiera('global_nodejs_version')
+        $nodejs_version = hiera('global_nodejs_version')
     }
     $nodejs_base_url = hiera('nodejs_base_url')
 
@@ -14,12 +14,34 @@ class oaeservice::deps::archive::nodejs {
         extension       => 'tar.gz',
     }
 
-    file { "node_symlink":
-      path => "/usr/local/$nodejs_version",
-      ensure => link,
-      target => "/usr/local/node-$nodejs_version-linux-x64",
+    file { "node_dir_symlink":
+        path   => "/usr/local/$nodejs_version",
+        ensure => link,
+        target => "/usr/local/node-$nodejs_version-linux-x64",
     }
 
+    file { "node_symlink":
+        path   => "/usr/bin/node",
+        ensure => link,
+        target => "/etc/alternatives/node",
+    }
+
+    file { "node_alternative_symlink":
+        path   => "/etc/alternatives/node",
+        ensure => link,
+        target => "/usr/local/$nodejs_version/bin/node",
+    }
+
+    file { "npm_symlink":
+        path   => "/usr/bin/npm",
+        ensure => link,
+        target => "/etc/alternatives/npm",
+    }
+
+    file { "npm_alternative_symlink":
+        path   => "/etc/alternatives/npm",
+        ensure => link,
+        target => "/usr/local/$nodejs_version/bin/npm",
+    }
 
 }
-
